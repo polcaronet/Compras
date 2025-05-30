d2b62f2c1c10e2895fc1262d7ee2de1e0a9d0009
Anselmo Polcaro <polcaronet@gmail.com>
2025-05-30 18:13:37 -0300

    componetns

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,41 +1,69 @@
-import { View, Image, TouchableOpacity, Text } from "react-native";
-import { Button } from "@/components/Button";
-import { Input } from "@/components/Input";
-import { FilterStatus } from "@/types/FilterStatus";
-import { Filter } from "@/components/Filter";
+import { FlatList, Image, ScrollView, Text, TouchableOpacity, View } from "react-native"
 
-import { styles } from "./styles";
-import { Item } from "@/components/Item";
+import { styles } from "./styles"
+import { Button } from "@/components/Button"
+import { Input } from "@/components/Input"
+import { Filter } from "@/components/Filter"
+import { FilterStatus } from "@/types/FilterStatus"
+import { Item } from "@/components/Item"
 
-const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
-
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
+const ITEMS = [
+  {
+    id: "1",
+    status: FilterStatus.DONE,
+    description: "1 pacote de café"
+  },
+  {
+    id: "2",
+    status: FilterStatus.PENDING,
+    description: "3 pacotes de macarrão"
+  },
+  {
+    id: "3",
+    status: FilterStatus.PENDING,
+    description: "3 cebolas"
+  },
+]
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image source={require("@/assets/logo.png")}
-        style={styles.logo}
-      />
-      <View style={styles.form}>
-        <Input placeholder="O que você deseja comprar?" />
-        <Button title="Adicionar" />
+      <Image source={require('@/assets/logo.png')} style={styles.logo} />
+
+      <View style={styles.form}
+      >
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" />
       </View>
+
       <View style={styles.content}>
         <View style={styles.header}>
-          {
-            FILTER_STATUS.map((status) => (
-              <Filter key={status} status={status} isActive />
-            ))}
+          {FILTER_STATUS.map((status) => (
+            <Filter key={status} status={status} isActive />
+          ))}
 
           <TouchableOpacity style={styles.clearButton}>
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
-        <Item data={{ status: FilterStatus.DONE, description: 'Banana' }}
-          onStatus={() => console.log('Mudar o Status')}
-          onRemove={() => console.log('Remover')}
+
+        <FlatList 
+          data={[]}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Item
+              data={item} 
+              onStatus={() => console.log("mudar o status")}
+              onRemove={() => console.log("remover")}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={() => <Text style={styles.empty}>Nenhum item aqui.</Text>}
         />
       </View>
     </View>
-  );
+  )
 }
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,6 +4,7 @@ import { styles } from "./styles";
 export function Input({ ...rest }: TextInputProps) {
   return <TextInput
     style={styles.container}
+    placeholderTextColor="#74798B"
     {...rest}
   />;
 }
