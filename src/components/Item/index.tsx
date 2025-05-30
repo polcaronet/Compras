eeeba69dd42d4c9c85f62d2f995b30cc6705f4e3
Anselmo Polcaro <polcaronet@gmail.com>
2025-05-30 11:55:54 -0300

    mais updates

---
+++ src/components/Item/index.tsx
@@ -0,0 +1,32 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { Trash2 } from "lucide-react-native";
+import { styles } from "./styles";
+import { StatusIcon } from "../StatusIcon";
+import { FilterStatus } from "@/types/FilterStatus";
+
+type ItemData = {
+  status: FilterStatus;
+  description: string;
+};
+
+type Props = {
+  data: ItemData;
+  onStatus: () => void;
+  onRemove: () => void;
+};
+
+export function Item({ data, onStatus, onRemove }: Props) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity activeOpacity={0.8} onPress={onStatus}>
+        <StatusIcon status={data.status} />
+      </TouchableOpacity>
+      <Text style={styles.description}>
+        {data.description}
+      </Text>
+      <TouchableOpacity activeOpacity={0.8} onPress={onRemove}>
+        <Trash2 size={18} color="#828282" />
+      </TouchableOpacity>
+    </View>
+  );
+}
