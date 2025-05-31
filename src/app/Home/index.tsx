b972844f9bb2a1df0d54843b02d9e6ff710ba788
Anselmo Polcaro <polcaronet@gmail.com>
2025-05-31 10:38:12 -0300

    update do input metodo remove

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -45,6 +45,16 @@ export function Home() {
     }
   }
 
+  async function handleRemove(id: string) {
+    try {
+      await itemsStorage.remove(id)
+      await itemsByStatus()
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Erro", "Não foi possível remover.")
+    }
+  }
+
   useEffect(() => {
     itemsByStatus()
   }, [filter])
@@ -88,7 +98,7 @@ export function Home() {
             <Item
               data={item} 
               onStatus={() => console.log("mudar o status")}
-              onRemove={() => console.log("remover")}
+              onRemove={() => handleRemove(item.id)}
             />
           )}
           showsVerticalScrollIndicator={false}
