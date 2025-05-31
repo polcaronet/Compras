6a7ca0efb475624eac3425e7996f5cd653d026e6
Anselmo Polcaro <polcaronet@gmail.com>
2025-05-31 13:01:26 -0300

    updates

--- src/storage/itemsStorage.ts
+++ src/storage/itemsStorage.ts
@@ -54,10 +54,27 @@ async function clear(): Promise<void> {
   }
 }
 
+async function toggeStatus(id: string): Promise<void> {
+  const items = await get()
+  const updatedItems = items.map((item) => 
+    item.id === id ?
+      {
+        ...item,
+        status:
+          item.status === FilterStatus.PENDING
+            ? FilterStatus.DONE
+            : FilterStatus.PENDING
+      }
+      : item
+  )
+  await save(updatedItems)
+}
+
 export const itemsStorage = {
   get,
   getByStatus,
   add,
   remove,
   clear,
+  toggeStatus,
 }
